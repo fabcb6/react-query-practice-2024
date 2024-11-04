e2a7a86c750a5bdf56a63b9a22ae863e1fa45880
Fabian Campos <fcampos@icapitalnetwork.com>
2024-11-04 16:00:31 -0600

    Setting app the react-query tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import PokemonsList from './pokemons/PokemonsList';
 import PokemonItem from './pokemon/PokemonItem';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<PokemonsList />} />
-        <Route path="/pokemon/:name" element={<PokemonItem />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<PokemonsList />} />
+          <Route path="/pokemon/:name" element={<PokemonItem />} />
+        </Routes>
+      </Router>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
--- src/pokemons/PokemonsList.tsx
+++ src/pokemons/PokemonsList.tsx
@@ -1,45 +1,34 @@
-// pokemon/PokemonItem.tsx
-import React from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import { useGetPokemon } from './useGetPokemon';
-import { CircularProgress, Card, CardContent, CardMedia, Typography, List, ListItem, Button } from '@mui/material';
+import { useGetPokemons } from './useGetPokemons';
+import { Link } from 'react-router-dom';
+import { CircularProgress, Stack, Card, CardMedia, CardContent, Typography } from '@mui/material';
 
-function PokemonItem() {
-  const { name } = useParams<{ name: string }>();
-  const navigate = useNavigate();
-  const { data, error, isLoading } = useGetPokemon(name as string);
+function PokemonsList() {
+  const { data, error, isLoading } = useGetPokemons();
   
   if (isLoading) return <CircularProgress />;
   if (error) return <p>Error: {error.message}</p>;
   
   return (
-    <>
-      <Button onClick={() => navigate(-1)} variant="contained" color="primary" style={{ margin: '20px' }}>
-        Back
-      </Button>
-      <Card style={{ maxWidth: '400px', margin: '20px auto', padding: '10px' }}>
-        <CardMedia
-          component="img"
-          height="400"
-          image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data?.id}.png`}
-          alt={data?.name}
-        />
-        <CardContent>
-          <Typography variant="h5" component="div" textAlign="center">
-            {data?.name}
-          </Typography>
-          <Typography variant="h6" component="div" marginTop="20px">
-            Moves:
-          </Typography>
-          <List>
-            {data?.moves.map((move, index) => (
-              <ListItem key={index}>{move.move.name}</ListItem>
-            ))}
-          </List>
-        </CardContent>
-      </Card>
-    </>
+    <Stack direction="row" flexWrap="wrap" justifyContent="center" style={{ padding: '20px' }}>
+      {data?.results.map((pokemon) => (
+        <Link to={`/pokemon/${pokemon.name}`} key={pokemon.id} style={{ textDecoration: 'none' }}>
+          <Card style={{ width: '200px', margin: '10px' }}>
+            <CardMedia
+              component="img"
+              height="200"
+              image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+              alt={pokemon.name}
+            />
+            <CardContent>
+              <Typography variant="h5" component="div" textAlign="center">
+                {pokemon.name}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Link>
+      ))}
+    </Stack>
   );
 }
 
-export default PokemonItem;
+export default PokemonsList;
